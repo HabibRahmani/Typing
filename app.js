3d45f6dc9213ba5fc6ef2505a6569f414c9d4f38
habib rahmani <habibrahmani512@gmail.com>
2023-03-25 15:27:00 +0430

    add lessons

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('test');
+const lessons = document.querySelectorAll('.lesson');
+const lessonText = document.querySelector('.lesson-text');
+
+const exercises = [
+    "asl; ll aa ss ;; all saa a;; ll aa llss aa;; ssll llaa ss;; ssaa aa ;; ll llaa ll;; ssaa aa;; llss aa;; ss aass llss;; asl; ll aa ss ;; all saa a;; ll aa llss aa;; ssll llaa ss;; ssaa aa ;; ll llaa ll;; ssaa aa;; ;;ss aall ss aass llss;; asl; ll aa ss ;; all saa a;; ll aa llss aa;; ssll llaa ss;; ssaa aa ;; ll llaa ll;; ssaa aal; ;;ss aass ss aass llss;; asl;",
+    "df jk dddd jjkk dkjf fjkd fjj kdd kkaa all; dddd fjj kk ssll fjkl asdf jkl; df jk dddd jjkk dkjf fjkd fjj kdd kkaa all; dddd fjj kk ssll fjkl asdf jkl; df jk dddd jjkk dkjf fjkd fjj kdd kkaa all; dddd fjj kk ssll fjkl asdf jkl; df jk dddd jjkk dkjf fjkd fjj kdd kkaa all; dddd fjj kk ssll fjkl asdf jkl; df jk dddd jjkk dkjf fjkd fjj kdd kkaa all; dddd fjj kk ssll fjkl asdf jkl;",
+    "3","4","5","6","7","8","9","10","11","12",];
+
+for(let i = 0; i < lessons.length; i++){
+    lessons[i].addEventListener("click", function() {
+        lessonText.textContent = exercises[i];
+    })
+}
