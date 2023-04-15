985ee8be279bc1fe7c3a0ad9b3a27a40668f2ebe
habib rahmani <habibrahmani512@gmail.com>
2023-04-15 16:12:43 +0430

    add game

---
+++ games/game-2.app.js
@@ -0,0 +1,6 @@
+const topicText = document.querySelector('.topic-text');
+
+const paragraphs = ["Black hole is a region of spacetime where gravity is so strong that nothing, including light or other electromagnetic waves, has enough energy to escape its event horizon. The theory of general relativity predicts that a sufficiently compact mass can deform spacetime to form a black hole.",
+"Galaxy is a system of stars, stellar remnants, interstellar gas, dust, dark matter, bound together by gravity. The word is derived from the Greek galaxias, literally 'milky', a reference to the Milky Way galaxy that contains the Solar System.",
+"Darwinism is a theory of biological evolution developed by the English naturalist Charles Darwin and others, stating that all species of organisms arise and develop through the natural selection of small, inherited variations that increase the individual's ability to compete, survive, and reproduce.",
+];
