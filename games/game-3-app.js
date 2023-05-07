766e5a09e5a8855632287834dae981a10f5fd5dc
habib rahmani <habibrahmani512@gmail.com>
2023-05-07 22:08:17 +0430

    add game

--- games/game-3-app.js
+++ games/game-3-app.js
@@ -1,4 +1,4 @@
-const doors = document.querySelector('.doors');
+let doors = document.querySelector('.doors');
 const human = document.querySelector('.human');
 const ghost = document.querySelector('.ghost');
 
@@ -21,3 +21,35 @@ function run (){
     }
 }
 // ######################################
+// ############# Doors ################
+let addedDoors;
+let doorTimer;
+let startPoint = 1030;
+
+
+
+ doorTimer = setInterval(addDoor, 100);
+
+
+
+
+function addDoor (leftPosition){
+    let door = document.createElement('div');
+    door.classList.add('door');
+    door.style.left = startPoint+"px"
+    doors.appendChild(door);
+
+    doorsPosition()
+}
+
+function doorsPosition (){
+    addedDoors = document.querySelectorAll('.doors');
+
+
+
+    for(let i = addedDoors.length; i >= 0 ; i++){
+     addedDoors[i].style.left = (1030 - (i * 10)) +"px";
+    }
+
+}
+
