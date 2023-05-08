03d90d494446271103c68f23369d472bb114c049
habib rahmani <habibrahmani512@gmail.com>
2023-05-08 22:03:51 +0430

    add game

--- games/game-3-app.js
+++ games/game-3-app.js
@@ -2,6 +2,8 @@ let doors = document.querySelector('.doors');
 const human = document.querySelector('.human');
 const ghost = document.querySelector('.ghost');
 
+const words = ["book","table","desk","pen","car","home","picture","window","door","road","ship","mobile","stone","hand","eye","head","tongue","finger","knee"];
+
 
 // ############ Scary Man ############# //
 let runnigHumanTimer;
@@ -37,12 +39,13 @@ function addDoor (leftPosition){
     door.style.left = startPoint+"px"
     doors.appendChild(door);
 
+    addWord(door);
 
 }
 function doorsPosition (){
     addingTime++
     
-    if(addingTime % 20 === 0){
+    if(addingTime % 25 === 0){
         addDoor()
     }
     
@@ -59,4 +62,22 @@ function doorsPosition (){
          addedDoors[i].style.left = parseInt(addedDoors[i].style.left) - 10 +"px";
     }
 }
+// ################ WORDS ##############
 
+
+function addWord (door){
+
+    let word = document.createElement('div');
+    word.classList.add('word');
+    door.appendChild(word);
+
+    let randomWord = Math.floor(Math.random() * words.length);
+
+    for(let i = 0; i < words[randomWord].length; i++){
+        let letter = document.createElement('div');
+        letter.classList.add('letter');
+        letter.textContent = words[randomWord][i];
+        word.appendChild(letter);
+    }
+
+}
