deee2b39a3acf67c2d06ad1ab0d0384cdf50e3f4
habib rahmani <habibrahmani512@gmail.com>
2023-05-09 22:17:45 +0430

    add game

--- games/game-3-app.js
+++ games/game-3-app.js
@@ -2,7 +2,21 @@ let doors = document.querySelector('.doors');
 const human = document.querySelector('.human');
 const ghost = document.querySelector('.ghost');
 
-const words = ["book","table","desk","pen","car","home","picture","window","door","road","ship","mobile","stone","hand","eye","head","tongue","finger","knee"];
+const words = ["book", "table", "desk", "pen", "car", "home", "picture", "window", "door", "road", "ship", "mobile", "stone", "hand", "eye", "head", "tongue", "finger", "knee"];
+
+
+// ######### Key Press ########3
+let value;
+let currentLetter = 0;
+let currentWordLenght;
+
+
+document.addEventListener('keypress', (e) => {
+    value = e.key;
+
+    checkWrittenLetters()
+
+})
 
 
 // ############ Scary Man ############# //
@@ -11,73 +25,65 @@ let runnigHumanTimer;
 let runnigHuman = true;
 runnigHumanTimer = setInterval(run, 150);
 
-function run (){
-    if(runnigHuman){
+function run() {
+    if (runnigHuman) {
         human.style.backgroundImage = "url(../icons/fast-running-human.svg) "
         ghost.style.top = "158px"
         runnigHuman = false;
-    }else{
+    } else {
         human.style.backgroundImage = "url(../icons/walking-human.svg) "
         ghost.style.top = "160px"
         runnigHuman = true;
     }
 }
-// ######################################
+// ####################################
 // ############# Doors ################
 let addedDoors;
 let doorTimer;
 let startPoint = 1030;
 let addingTime = 0;
 
-
 addDoor()
 doorTimer = setInterval(doorsPosition, 100);
 
-function addDoor (leftPosition){
+function addDoor(leftPosition) {
     let door = document.createElement('div');
     door.classList.add('door');
-    door.style.left = startPoint+"px"
+    door.style.left = startPoint + "px"
     doors.appendChild(door);
 
-    addWord(door);
-
 }
-function doorsPosition (){
+function doorsPosition() {
     addingTime++
-    
-    if(addingTime % 25 === 0){
+
+    if (addingTime % 25 === 0) {
         addDoor()
     }
-    
+
     addedDoors = document.querySelectorAll('.door');
 
-     
-    for(let i = addedDoors.length - 1; i >= 0 ; i--){
 
-    if(parseInt(addedDoors[i].style.left) < 250){
-        addedDoors[i].remove()
+    for (let i = addedDoors.length - 1; i >= 0; i--) {
 
-    }
+        if (parseInt(addedDoors[i].style.left) < 250) {
+            addedDoors[i].remove()
 
-         addedDoors[i].style.left = parseInt(addedDoors[i].style.left) - 10 +"px";
+        }
+
+        addedDoors[i].style.left = parseInt(addedDoors[i].style.left) - 10 + "px";
     }
 }
 // ################ WORDS ##############
 
 
-function addWord (door){
-
-    let word = document.createElement('div');
-    word.classList.add('word');
-    door.appendChild(word);
-
+function addWord(door) {
+    
     let randomWord = Math.floor(Math.random() * words.length);
 
-    for(let i = 0; i < words[randomWord].length; i++){
+    for (let i = 0; i < words[randomWord].length; i++) {
         let letter = document.createElement('div');
         letter.classList.add('letter');
         letter.textContent = words[randomWord][i];
-        word.appendChild(letter);
+        door.appendChild(letter);
     }
-
 }
