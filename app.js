01e07e885b2a84122ba88da3ffbb8c3d0dd55b01
habib rahmani <habibrahmani512@gmail.com>
2023-05-23 22:44:25 +0430

    solve mistakes

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function boxesEventListener() {
             if (i <= opendLessons) {
                 addLesson(i)
                 currentLesson = i;
-                englishPage.style.background = "url(./images/typing-cats.webp)no-repeat fixed center"
+                englishPage.style.background = "url(./images/typing-avif-2.avif)no-repeat fixed center"
                 englishPage.style.backgroundSize = "100%"
                 exercisePage.style.display = "block"
                 lessonsBody.style.display = "none"
