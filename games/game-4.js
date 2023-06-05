19f7c181147fab97027de13f1b9275036910d553
habib rahmani <habibrahmani512@gmail.com>
2023-06-05 11:23:33 +0430

    add new game

---
+++ games/game-4.js
@@ -0,0 +1,5 @@
+const text = document.querySelector('.text')
+
+
+const paragraphs = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusamus consequatur esse a omnis exercitationem."]
+
